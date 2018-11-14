8dee9ad3d716a5b3e85de6f2ae8e589aced13d8e
mehmeteminduran <mehmet3646@hotmail.com>
2018-11-14 18:23:43 +0300

    refactor

--- js/app.js
+++ js/app.js
@@ -1,40 +1,124 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+// canvasHeight = 606;
+// numRows = 6,
+// numCols = 5,
+// col * 101, row * 83
+// player width 101,height 171
+let maxSpeed = 400, canvasHeight = 606, canvasWidth = 505, numRows = 6, numCols = 5, columnWidth = 101, rowHeight = 83, playerXStart = 202.5, playerYStart = 383;
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+class Enemy {
+    constructor(x, y, speed) {
+        // Variables applied to each of our instances go here,
+        // we've provided one for you to get started
+
+        // The image/sprite for our enemies, this uses
+        // a helper we've provided to easily load images
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.sprite = 'images/enemy-bug.png';
+    }
+
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+
+    update(dt) {
+        this.x += this.speed * dt;
+        if (this.x > canvasWidth) {
+            this.x = -100;
+            this.getRandomSpeed();
+        }
+
+        this.checkCollision();
+
+    }
+
+    getRandomSpeed() {
+        this.speed = 100 + Math.floor(Math.random() * (maxSpeed - 99));
+    };
+
+    checkCollision() {
+        const playerWidth = 50, playerHeight = 60, enemyWidth = 60, enemyHeight = 50;
+        if (player.x < this.x + enemyWidth &&
+            player.x + playerWidth > this.x &&
+            player.y < this.y + enemyHeight &&
+            playerHeight + player.y > this.y) {
+            console.log('collision');
+            player.reset(); 
+        }
+    }
+
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
+
+class Player {
+    constructor(x, y, speed) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.sprite = 'images/char-pink-girl.png';
+    }
+
+    update() { }
+
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    handleInput(key) {
+
+        switch (key) {
+            case 'left':
+                if (this.x > columnWidth) {
+                    this.x -= columnWidth;
+                }
+                break;
+            case 'up':
+                if (this.y < 0) {
+                    alert('*** CONGRATULATIONS ***');
+                    player.reset(); 
+                } else {
+                    this.y -= rowHeight;
+                }
+
+                break;
+            case 'down':
+                if (this.y + rowHeight <= 450)
+                    this.y += rowHeight;
+                break;
+            case 'right':
+                if (this.x + columnWidth <= canvasWidth) {
+                    this.x += columnWidth;
+                }
+                break;
+        }
+    }
+
+    reset() {
+        this.x = playerXStart;
+        this.y = playerYStart;
+    }
+}
+ 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+let allEnemies = [new Enemy(0, 60, 50), new Enemy(170, 140, 90), new Enemy(80, 225, 50), new Enemy(250, 60, 60), new Enemy(310, 140, 70)];
+// Canvas position of created enemies and player x, y, movement 
+let player = new Player(playerXStart, playerYStart, 50);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
